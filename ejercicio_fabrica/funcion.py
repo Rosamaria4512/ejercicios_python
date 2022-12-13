4c246f7d5f2a95f8b3ef046144a147050d4b053c
rmquilindo8 <rmquilindo8@misena.edu.co>
2022-12-13 16:59:26 -0500

    ejercicios Python

---
+++ ejercicio_fabrica/funcion.py
@@ -0,0 +1,4 @@
+def holi(nombre,num):
+    print('hola mundo',nombre,num)
+
+holi("rosa",1)
---
+++ ejercicio_fabrica/funcion4_segundohoras.py
@@ -0,0 +1,12 @@
+def tiempo(segundos):
+    
+
+
+
+    tiempo=hora*60/1
+    x=minutos*tiempo/1
+    print (tiempo,'minutos')
+    print(x,'segundos')
+    y=tiempo+x+minutos
+    print(y,'tiempo en segundos')
+tiempo(5,6)
